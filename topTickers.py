3adb9252404a3b0d79829f176036a2de9a60049f
oasisks <oasisks2@gmail.com>
2024-03-26 00:13:46 -0400

    Added in a skeleton code for this

---
+++ topTickers.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import pprint
+
+
+def parser(file_dir: str):
+    """
+    Takes in a directory to a csv file containing the Finra Equity Short Interest Data Format
+    See: https://www.finra.org/finra-data/browse-catalog/equity-short-interest/data
+    :param file_dir: the directory to the file
+    :return: a list of stocks
+    """
+    pass
+
+
+def ticker_scraper():
+    pass
+
+
+def main():
+    print("I am in main")
+
+
+if __name__ == "__main__":
+    main()
